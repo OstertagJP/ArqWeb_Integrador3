fa1d5f4329ec790b6345808b0971753c0a92a499
katia-otot <katiagadea@gmail.com>
2025-05-19 22:35:48 -0300

    Falta g y h

--- src/main/java/com/arqweb/integrador3/controller/ReporteController.java
+++ src/main/java/com/arqweb/integrador3/controller/ReporteController.java
@@ -1,6 +1,8 @@
 package com.arqweb.integrador3.controller;
 
+import com.arqweb.integrador3.dto.CarreraDTO;
 import com.arqweb.integrador3.dto.ReporteCarreraDTO;
+import com.arqweb.integrador3.service.CarreraService;
 import com.arqweb.integrador3.service.ReporteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,10 +17,19 @@ public class ReporteController {
     @Autowired
     private ReporteService reporteService;
 
-    // f) Carreras con cantidad de inscriptos, ordenadas
+    @Autowired
+    private CarreraService carreraService;
+
+    // h) Reporte final
     @GetMapping("/carreras")
     public List<ReporteCarreraDTO> carrerasConInscriptos() {
 
         return reporteService.generarReporteCarreras();
+    }
+
+    // f) Carreras ordenadas por cantidad de inscriptos
+    @GetMapping("/carreras-inscriptos")
+    public List<CarreraDTO> carrerasOrdenadasPorInscriptos() {
+        return carreraService.obtenerCarrerasOrdenadasPorCantidadDeInscriptos();
     }
 }
